acb1db111fb3e2f3c7ad28c17cdcbee095f66f49
tenryou-17 <live_wire117@hotmail.com>
2015-01-30 18:34:33 -0500

    Colour Mixing Lamp
    Arduino controls the RGB to slowly mix colours creating a Mood Light.

---
+++ Colour_Mixing_Lamp1.ino
@@ -0,0 +1,62 @@
+//Name each output pin so it's easier to read
+const int greenLEDpin = 11;
+const int redLEDpin = 9;
+const int blueLEDpin = 10;
+
+//Declare variables for current state of button latch & previous state as well as button variable
+int state = LOW;
+int previous;
+int button;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2, INPUT);
+  pinMode(9, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  attachInterrupt(0, statechange, RISING);  //see this link for why: "arduino.cc/en/Reference/AttachInterrupt"
+    
+}
+
+void loop()
+{
+  unsigned int rgbColour[3];
+
+  if (state == HIGH)
+  {
+    // Start off with red.
+    rgbColour[0] = 255;
+    rgbColour[1] = 0;
+    rgbColour[2] = 0;  
+
+    // Choose the colours to increment and decrement.
+    for (int decColour = 0; decColour < 3; decColour += 1) 
+    {
+      int incColour = decColour == 2 ? 0 : decColour + 1;
+
+      // cross-fade the two colours.
+      for(int i = 0; i < 255; i += 1) 
+      {
+        rgbColour[decColour] -= 1;
+        rgbColour[incColour] += 1;
+
+        analogWrite(redLEDpin, rgbColour[0]);
+        analogWrite(greenLEDpin, rgbColour[1]);
+        analogWrite(blueLEDpin, rgbColour[2]);
+        delay(5);//change this to change how quickly colours fade in/out
+      }
+    }
+  }
+  else if (state == LOW) //LED is off if button state is low
+  {
+    analogWrite(redLEDpin, 0);
+    analogWrite(greenLEDpin, 0);
+    analogWrite(blueLEDpin, 0);
+  }
+}
+
+void statechange() //flip flops LED state
+{
+  state = !state;
+}
